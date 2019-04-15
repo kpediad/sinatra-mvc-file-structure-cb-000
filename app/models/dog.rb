3fb56b12d8108f3c4f7d3af6db57acdbc89d2fb9
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-04-15 13:10:51 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    self.name = name
+    self.breed = breed
+    self.age = age
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all.push(self)
+  end
+
+end
